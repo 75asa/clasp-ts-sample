a21dd7e396bbfe49b297f60252c05cd1ee00d4a3
Asato Nago <nago@tam-bourine.co.jp>
2019-09-08 23:21:33 +0900

    chore: invalid access token

--- src/Hello.ts
+++ src/Hello.ts
@@ -2,10 +2,14 @@ export const hoge = () => {
   console.log("hige");
 };
 
+export const foo = () => {
+  console.log("hhh");
+};
+
 function main() {
   const sheet = SpreadsheetApp.getActiveSpreadsheet().getActiveSheet();
   const today = Utilities.formatDate(new Date(), "JST", "yyyy/MM/DD");
-  const value = "Hello clasp ts";
+  const value = "Hello clasp ts !!!!";
 
   console.log(value);
   sheet.appendRow([today, value]);
